df81e045851e624c30d420937e8ac159861d6857
octaltree <octaltree@gmail.com>
2016-10-14 20:23:38 +0900

    comp

--- a.py
+++ a.py
@@ -3,22 +3,25 @@
 import sys
 import random
 import numpy as np
-from math import log
+import math
 undefined = None
 
 def main():
+    strs = lambda xs: [str(x) for x in xs]
+    mean = lambda xs: sum(xs) / len(xs)
     hm = HiddenMarkov()
     hm.read()
-    (string, sts) = hm.generate(100)
-    strs = lambda xs: [str(x) for x in xs]
-    print(' {0} '.format(string))
-    print(''.join(strs(sts)))
-    pred = hm.viterbi(string)
-    print(''.join(strs(pred)))
-    print(len(
-        [t for t in list(zip(sts, pred))[1:-1] if t[0] == t[1]]
-        ) / len(sts))
+    print(np.average(list(map(lambda i: generatePredict(hm)[3], range(1000)))))
     return 0
+
+def generatePredict(hm):
+    (string, sts) = hm.generate(100)
+    pred = hm.viterbi(string)
+    accuracy = 1 if len(pred) <= 2 else (
+            len([t for t in list(zip(sts, pred))[1:-1] if t[0] == t[1]]) /
+            (len(sts) - 2)
+            )
+    return (string, sts,  pred, accuracy)
 
 class HiddenMarkov:
     __alphs = None
@@ -88,31 +91,33 @@ class Viterbi:
         self.__delta = delta
     def __e(self, st, alph):
         return self.__states[st][''.join(self.__alphs).index(alph)]
+    def __log(self, x):
+        return -np.inf if x == 0 else math.log(x)
     def __fill(self, string):
-        for idx in range(1, len(string)):
+        for idx in range(len(string)):
             for st in range(1, self.__stnum - 1):
-                m = max([
-                    self.__dp[j][idx-1] + log(self.__delta[j][st])
+                m = np.max([
+                    self.__dp[j][idx] + self.__log(self.__delta[j][st])
                     for j in range(self.__stnum - 1)
                     ])
-                self.__dp[st][idx] = log(self.__e(st, string[idx])) + m
+                self.__dp[st][idx+1] = self.__log(self.__e(st, string[idx])) + m
     def __read(self, length):
         current = self.__stnum - 1
         path = []
-        for idx in reversed(range(length)):
+        for idx in reversed(range(length + 1)):
             path = [current] + path
             current = np.argmax([
                 self.__dp[st][idx] + self.__delta[st][current]
                 for st in range(self.__stnum - 1)
                 ])
+        path = [0] + path
         return path
     def predict(self, string):
         if self.__stnum == 2 or len(string) == 0:
             return [0, self.__stnum - 1]
-        self.__dp = np.full((self.__stnum, len(string)), np.nan)
+        self.__dp = np.full((self.__stnum, len(string)+1), -np.inf)
         self.__dp[0][0] = 0
         self.__fill(string)
-        print(self.__dp)
         return self.__read(len(string))
 
 if __name__ == "__main__":
