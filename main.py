f1967afc30484503687bfd4cc1d6cd41a04ef9ca
octaltree <octaltree@gmail.com>
2016-10-26 00:39:28 +0900

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python
 import bg
+import sys
 
 def main():
     hm = bg.HiddenMarkov()
     hm.read()
-    bg.task4(hm)
+    if len(sys.argv) < 2:
+        bg.task1(hm)
+        bg.task2(hm)
+        bg.task3(hm)
+        bg.task4(hm)
+    elif sys.argv[1] == "1":
+        bg.task1(hm)
+    elif sys.argv[1] == "2":
+        bg.task2(hm)
+    elif sys.argv[1] == "3":
+        bg.task3(hm)
+    elif sys.argv[1] == "4":
+        bg.task4(hm)
     return 0
 
 if __name__ == "__main__":
